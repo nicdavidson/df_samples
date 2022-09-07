ae661f0da49104052db49ef4e6ba6274b8b8f7a6
nicdavidson <ndavidson1717@#gmail.com>
2022-09-07 17:53:01 -0600

    finished ssn obfuscate

--- pqsql_demo/obfuscate_ssn.py
+++ pqsql_demo/obfuscate_ssn.py
@@ -1,13 +1,18 @@
-
 ### This is meant to be a post process script which will take a SSN field and obfuscate all but the last 4 numbers. 
 
 
 # field name as it appears in the database. 
-field_name = 'acct_social'
+field_name = 'acct_ssn'
 
-records = event.response.content
+records = event.response.content.resource
 
-
-for record in records:
-    print(record)
-    print('\n...\n')
+for record in records: # iterate through every record that was collected
+    try:
+        if not record[field_name]: # if the value of field_name is None (NULL)
+            record[field_name] = 'err' 
+        else:
+            record[field_name] = str(record[field_name])[-4:]
+    ## Generally not a best practice to catch every exception, but I'd rather do that than release a ssn. 
+    except Exception as e: # if there is an exception or issue, just make it an err, print the err to the log
+        print(str(e))
+        record[field_name] = 'err'
