cdbf9e1a89c18500bea86da1306ba6d172c05b53
nicdavidson <ndavidson1717@#gmail.com>
2022-09-07 15:22:57 -0600

    testing

--- pqsql_demo/obfuscate_ssn.py
+++ pqsql_demo/obfuscate_ssn.py
@@ -1,4 +1,10 @@
 
 ### This is meant to be a post process script which will take a SSN field and obfuscate all but the last 4 numbers. 
 
-print('hello')
+
+# field name as it appears in the database. 
+field_name = 'acct_social'
+
+records = event.request.content
+
+print(records)
